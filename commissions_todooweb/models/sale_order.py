57f9c48271a1459d2a4989efbd963ae0b70a1ee2
UserOdoo <tatiana.rosabal>
2021-05-31 18:38:17 +0200

    Nuew module

---
+++ commissions_todooweb/models/sale_order.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    dont_calculate = fields.Boolean(string='Dont Calculate Commission')
+    net_margin = fields.Float('Net Margin')
+    gross_margin = fields.Float('Net profit')
