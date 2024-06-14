25ff3de95817e46aa10ddbcd5e32967a2dcdd37f
Edgar <edgar.naranjo>
2024-06-14 13:26:48 +0200

    [Add] Check final_stage in ProjectProject

--- timesheet_portal_todoo/__manifest__.py
+++ timesheet_portal_todoo/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Timesheet Portal",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "category": "Website/Website",
 
     "summary": """
--- timesheet_portal_todoo/models/project_project.py
+++ timesheet_portal_todoo/models/project_project.py
@@ -6,3 +6,17 @@ class Project(models.Model):
     _inherit = 'project.project'
 
     allow_timesheet_portal = fields.Boolean('Allow portal timesheet')
+
+    @api.onchange('stage_id')
+    def onchange_stage_id(self):
+        if self.stage_id:
+            if self.stage_id.check_final_stage:
+                self.allow_timesheet_portal = True
+            else:
+                self.allow_timesheet_portal = False
+
+
+class ProjectProjectStage(models.Model):
+    _inherit = 'project.project.stage'
+
+    check_final_stage = fields.Boolean('Final stage')
