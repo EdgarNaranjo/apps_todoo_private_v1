98a77b1e4a4ec62402f65d3b8ed077c9318180e6
Edgar <edgar.naranjo>
2024-08-29 22:07:13 +0200

    [Add] Add new module analytic_line_validated_todoo

---
+++ analytic_line_validated_todoo/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Not Delete Line Validate",
+    'version': '16.0.1.0.0',
+    'summary': 'Do not delete analytic lines once they are validated.',
+    'description': """Do not delete analytic lines once they are validated.""",
+    'license': 'AGPL-3',
+    'author': "ToDOO Web (www.todooweb.com)",
+    'category': 'Services/Timesheets',
+    'website': "https://todooweb.com/",
+    'contributors': [
+        "Equipo Dev <devtodoo@gmail.com>",
+        "Edgar Naranjo <edgarnaranjof@gmail.com>",
+    ],
+    'support': 'devtodoo@gmail.com',
+    'depends': ['base', 'timesheet_grid'],
+    'images': [
+       'static/description/screenshot_time.png'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 5.99,
+    'currency': 'EUR',
+}
