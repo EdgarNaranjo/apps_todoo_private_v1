2fbfdcd1f849fd5eba58faf4b26f4148913814c3
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2024-09-28 17:30:13 -0400

    [17.0][MIG] pos_vantiv_tripos_cloud

--- pos_vantiv_tripos_cloud/models/__init__.py
+++ pos_vantiv_tripos_cloud/models/__init__.py
@@ -1,6 +1,5 @@
 from . import pos_vantiv_tripos_cloud
 from . import pos_vantiv_transaction
-from . import pos_order
 from . import pos_config
 from . import res_config_settings
 from . import pos_session
