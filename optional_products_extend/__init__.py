4cbdab53b4b0ce40cba8e5e76a07b7f76df7caee
Edgar Legrand <edgarlegrand@MacBook-Air-de-Edgar.local>
2025-07-23 23:25:37 +0200

    Fix in modules

--- optional_products_extend/__init__.py
+++ optional_products_extend/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024-TODAY Todooweb (www.todooweb.com)
+# Copyright 2025-TODAY Todooweb (www.todooweb.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from . import models
