ee5b04f4d1654a020d4ba8361ae2da31269ac441
EdgarNaranjo <legranddidous@gmail.com>
2026-05-17 00:09:02 +0200

    Add new module ban_user_todoo

---
+++ ban_user_todoo/__manifest__.py
@@ -0,0 +1,37 @@
+# Copyright 2025-TODAY Todooweb (https://todooweb.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    'name': "Ban/Block Users",
+    'version': '19.0.1.0.0',
+    'summary': """Ban/Block Users Configurable""",
+    'description': """Ban/Block Users from Backend.""",
+    'license': 'LGPL-3',
+    'author': "ToDOO Web (www.todooweb.com)",
+    'category': 'Extra Tools',
+    'website': "https://todooweb.com/",
+    'contributors': [
+        "Equipo Dev <devtodoo@gmail.com>",
+        "Edgar Naranjo <edgarnaranjof@gmail.com>",
+    ],
+    'support': 'devtodoo@gmail.com',
+    'depends': [
+        'base',
+        'mail',
+        'web',
+        'bus'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_users_views.xml',
+        'wizard/ban_user_wizard_views.xml',
+    ],
+    'images': [
+      'static/description/screenshot_blocked.png'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 19.99,
+    'currency': 'EUR',
+}
+
---
+++ ban_user_todoo/wizard/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2025-TODAY Todooweb (https://todooweb.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from . import ban_user_wizard
